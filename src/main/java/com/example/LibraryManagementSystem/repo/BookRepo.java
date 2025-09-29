6cb089e37d52af3c4b0d7324df8e1fedf6bf1b40
Anas <anasmony15@gmail.com>
2025-09-30 00:50:21 +0300

    modified in the book controller for routing adjusments

--- src/main/java/com/example/LibraryManagementSystem/repo/BookRepo.java
+++ src/main/java/com/example/LibraryManagementSystem/repo/BookRepo.java
@@ -2,11 +2,10 @@ package com.example.LibraryManagementSystem.repo;
 
 import com.example.LibraryManagementSystem.model.entity.BookEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 
+@Repository
 public interface BookRepo extends JpaRepository<BookEntity, Integer> {
     BookEntity findByIsbn(String isbn);
 
